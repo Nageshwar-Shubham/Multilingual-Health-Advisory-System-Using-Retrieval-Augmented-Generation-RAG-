589a772e2a0d749eb333499843dff1c17c68fe88
Nageshwar-Shubham <nageshwarkumar614378@gmail.com>
2026-06-12 17:48:18 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ="Multilingual Health Advisory System",
+    version="1.0.0",
+    author="Nageshwar Shubham",
+    packages=find_packages(),
+    install_requires=[]
+)
